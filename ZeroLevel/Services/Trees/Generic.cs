0fad8ca7f5445a3335b271d93a9d572165e58e2f
a.bozhenov <ogoun@users.noreply.github.com>
2019-09-20 16:40:35 +0300

    Update Generic.cs

--- ZeroLevel/Services/Trees/Generic.cs
+++ ZeroLevel/Services/Trees/Generic.cs
@@ -1,83 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace ZeroLevel.Services.Trees
 {
-    public class Tree<T>
-    {
-        private readonly Dictionary<string, TreeNode<T>> _rootNodes = new Dictionary<string, TreeNode<T>>();
-
-        public IEnumerable<TreeNode<T>> RootNodes
-        {
-            get
-            {
-                return _rootNodes.Values.ToArray();
-            }
-        }
-
-        public bool TryAdd(string id, T value, bool override_if_exists)
-        {
-            if (_rootNodes.ContainsKey(id))
-            {
-                if (override_if_exists)
-                {
-                    (_rootNodes[id].Value as IDisposable)?.Dispose();
-                    _rootNodes[id] = new TreeNode<T> { Value = value };
-                    return true;
-                }
-                return false;
-            }
-            _rootNodes[id] = new TreeNode<T> { Value = value };
-            return true;
-        }
-
-        private bool __TryAdd(string[] path, int index, T value, bool override_if_exists)
-        {
-            /* if (_rootNodes.ContainsKey(path[index]))
-             {
-             }*/
-            return false;
-        }
-
-        public T Find(string[] path)
-        {
-            return default(T);
-        }
-
-        public bool TryAdd(string[] path, T value, bool override_if_exists)
-        {
-            if (path.Length == 0) return false;
-            return __TryAdd(path, 0, value, override_if_exists);
-        }
-
-        public bool TryRemove(string id)
-        {
-            if (_rootNodes.ContainsKey(id))
-            {
-                (_rootNodes[id].Value as IDisposable)?.Dispose();
-                _rootNodes[id].SubNodes?.Clear();
-                _rootNodes.Remove(id);
-                return true;
-            }
-            return false;
-        }
-
-        public void Clear()
-        {
-            foreach (var key in _rootNodes.Keys)
-            {
-                (_rootNodes[key].Value as IDisposable)?.Dispose();
-                _rootNodes[key].SubNodes?.Clear();
-            }
-            _rootNodes.Clear();
-        }
-    }
-
-    public class TreeNode<T>
+    public class GNode<T>
     {
         public string Id { get; set; }
         public T Value { get; set; }
-        public Tree<T> SubNodes = null;
+        public readonly Dictionary<string, GNode<T>> Nodes = new Dictionary<string, GNode<T>>();
+
+        public void Add(string[] path, int index, T value)
+        {
+            if (path.Length > index)
+            {
+                if (false == Nodes.ContainsKey(path[index]))
+                {
+                    Nodes[path[index]] = new GNode<T>();
+                }
+                if (path.Length == (index + 1))
+                {
+                    Nodes[path[index]].Value = value;
+                }
+                else
+                {
+                    Nodes[path[index]].Add(path, index + 1, value);
+                }
+            }
+        }
+    }
+
+    public class GTree<T>
+    {
+        private readonly Dictionary<string, GNode<T>> _rootNodes = new Dictionary<string, GNode<T>>();
+        public IEnumerable<GNode<T>> RootNodes => _rootNodes.Values.ToArray();
+
+        public void Add(string[] path, T value)
+        {
+            if (path.Length > 0)
+            {
+                if (false == _rootNodes.ContainsKey(path[0]))
+                {
+                    _rootNodes[path[0]] = new GNode<T>();
+                }
+                if (path.Length == 1)
+                {
+                    _rootNodes[path[0]].Value = value;
+                }
+                else
+                {
+                    _rootNodes[path[0]].Add(path, 1, value);
+                }
+            }
+        }
     }
 }
