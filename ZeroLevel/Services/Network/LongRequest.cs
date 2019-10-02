88e932ca96438c5d135d19119766f193ce100c7b
a.bozhenov <ogoun@users.noreply.github.com>
2019-10-02 21:09:18 +0300

    Create LongRequest.cs

---
+++ ZeroLevel/Services/Network/LongRequest.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading;
+using ZeroLevel.Services.Serialization;
+
+namespace ZeroLevel.Services.Network
+{
+    public class LongRequest<T>
+        : IBinarySerializable
+    {
+        static long _index = 0;
+
+        public LongRequest() { }
+
+        public LongRequest<T> Create(T value, string serviceKey, string inbox) => new LongRequest<T>
+        {
+            Identity = Interlocked.Increment(ref _index),
+            Body = value,
+            ServiceKey = serviceKey,
+            Inbox = inbox
+        };
+
+        public long Identity { get; set; }
+        public T Body { get; set; }
+        public string ServiceKey { get; set; }
+        public string Inbox { get; set; }
+
+        public void Deserialize(IBinaryReader reader)
+        {
+            this.Identity = reader.ReadLong();
+            this.Inbox = reader.ReadString();
+            this.ServiceKey = reader.ReadString();
+            this.Body = reader.ReadCompatible<T>();
+        }
+
+        public void Serialize(IBinaryWriter writer)
+        {
+            writer.WriteLong(this.Identity);
+            writer.WriteString(this.Inbox);
+            writer.WriteString(this.ServiceKey);
+            writer.WriteCompatible<T>(this.Body);
+        }
+    }
+}
