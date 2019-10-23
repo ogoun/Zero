aa1528c018bb709a9e50a08fb35028aeca7f4cfd
a.bozhenov <ogoun@users.noreply.github.com>
2019-10-23 22:33:52 +0300

    append

---
+++ ZeroLevel/Services/Utils/RRandom.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace ZeroLevel.Services.Utils
+{
+    public static class RRandom
+    {
+        private static RNGCryptoServiceProvider Random = new RNGCryptoServiceProvider();
+
+        public static int RandomInteger(int min, int max)
+        {
+            uint scale = uint.MaxValue;
+            while (scale == uint.MaxValue)
+            {
+                byte[] four_bytes = new byte[4];
+                Random.GetBytes(four_bytes);
+                scale = BitConverter.ToUInt32(four_bytes, 0);
+            }
+            return (int)(min + (max - min) * (scale / (double)uint.MaxValue));
+        }
+
+        public static bool OverLimit(int limit)
+        {
+            return RandomInteger(0, 1000) > limit;
+        }
+    }
+}
