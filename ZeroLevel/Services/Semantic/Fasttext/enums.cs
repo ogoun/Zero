c3619be068649b0bfeb87980dd3672da8429dc96
Ogoun <ogoun@users.noreply.github.com>
2020-03-25 19:06:39 +0300

    update

--- ZeroLevel/Services/Semantic/Fasttext/enums.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ZeroLevel.Services.Semantic.Fasttext
-{
-    public enum model_name : int { cbow = 1, sg, sup };
-    public enum loss_name : int { hs = 1, ns, softmax };
-    public enum entry_type : byte { word=0, label=1};
-}
