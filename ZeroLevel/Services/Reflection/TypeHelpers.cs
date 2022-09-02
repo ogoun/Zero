3b3224b9314ea08ffe6e684d1f8640b81bcf3ca6
Ogoun <ogoun@users.noreply.github.com>
2022-09-02 10:07:07 +0300

    Update TypeHelpers.cs

--- ZeroLevel/Services/Reflection/TypeHelpers.cs
+++ ZeroLevel/Services/Reflection/TypeHelpers.cs
@@ -47,28 +47,6 @@ namespace ZeroLevel.Services.Reflection
                     return false;
             }
         }
-
-        public static bool IsNumericType(Type type)
-        {
-            switch (Type.GetTypeCode(type))
-            {
-                case TypeCode.Byte:
-                case TypeCode.SByte:
-                case TypeCode.UInt16:
-                case TypeCode.UInt32:
-                case TypeCode.UInt64:
-                case TypeCode.Int16:
-                case TypeCode.Int32:
-                case TypeCode.Int64:
-                case TypeCode.Decimal:
-                case TypeCode.Double:
-                case TypeCode.Single:
-                    return true;
-                default:
-                    return false;
-            }
-        }
-
         public static bool IsNumericTypeWithFloating(Type type)
         {
             switch (Type.GetTypeCode(type))
